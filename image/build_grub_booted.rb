72821a20986cb788e27d09a6e25b0961c62a3d6d
HfCloud <sxysxygm@gmail.com>
2019-08-08 19:04:19 +0800

    修改脚本自动下载template.img

--- image/build_grub_booted.rb
+++ image/build_grub_booted.rb
@@ -4,6 +4,11 @@
 require 'fileutils'
 Dir.chdir(File.dirname(__FILE__))
 
+puts("template.img not found, now download it.")
+if !File.exist?("template.img")
+    system("wget https://github.com/sxysxy/Taurix/releases/download/v0.11/template.img")
+end
+
 FileUtils.cp("template.img", "Taurix.img")
 if !system("tfstools Taurix.tfs > ImageBuild.log")
     FileUtils.rm("Taurix.img")
