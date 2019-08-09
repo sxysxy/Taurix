f01d026c637673f091524bc0056cf6b244a8977e
HfCloud <sxysxygm@gmail.com>
2019-08-09 22:18:38 +0800

    修复了一个build时候的bug

--- image/build_grub_booted.rb
+++ image/build_grub_booted.rb
@@ -4,16 +4,14 @@
 require 'fileutils'
 Dir.chdir(File.dirname(__FILE__))
 
-if !File.exist?("template.img")
-        
-    if RUBY_PLATFORM =~ /mingw/ || RUBY_PLATFORM =~ /mswin/
-        FileUtils.cp("../toolchain/template.img", "Taurix.img")
-    else 
+if RUBY_PLATFORM =~ /mingw/ || RUBY_PLATFORM =~ /mswin/
+    FileUtils.cp("../toolchain/template.img", "Taurix.img")
+else 
+    if !File.exist?("template.img")
         puts("template.img not found, now download it.")
         system("wget https://github.com/sxysxy/Taurix/releases/download/v0.11/template.img")
-        FileUtils.cp("template.img", "Taurix.img")
     end
-   
+    FileUtils.cp("template.img", "Taurix.img")
 end
 
 if !system("tfstools Taurix.tfs > ImageBuild.log")
